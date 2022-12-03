84317b63cce406b21445e15979b282e7a78877e2
carmenjoy <67772382+cjp-engr@users.noreply.github.com>
2022-12-03 13:26:42 +0800

    fourth commit

--- cypress/e2e/inventory.cy.ts
+++ cypress/e2e/inventory.cy.ts
@@ -1,9 +1,8 @@
 import { InventoryPage } from "../../cypress/pages/Inventory";
 import { ValidCredentials } from "./model";
 
-
-describe('', () => {
-    before(() => {
+describe('Add to cart and remove products scenario', () => {
+    beforeEach(() => {
         InventoryPage.visit();
         cy.fixture('login').as('login')
         cy.get<ValidCredentials>('@login').then((user) => {
@@ -12,32 +11,22 @@ describe('', () => {
     })
 
     it('Successfully changed button text from "ADD TO CART" to "REMOVE" after clicking the "ADD TO CART" button', () => {
+        InventoryPage.backpackAddToCartElement.contains('Add to cart');
+        InventoryPage.backpackAddToCartElement.click();
+        InventoryPage.backpackRemoveElement.should(($buttonText) => {
+            expect($buttonText).to.contain('Remove');
+        });
 
     });
 
     it('Successfully changed button text from "REMOVE" to "ADD TO CART" after clicking the "REMOVE" button', () => {
-
-
-    });
-
-    it('Successfully arranged the products from low to high price after selecting the "Price (low TO high)"', () => {
-
-
-    });
-
-    it('Successfully arranged the products from high to low price after selecting the "Price (high TO low)"', () => {
-
-
-    });
-
-    it('Successfully arranged the products from a to z after selecting the "Name (A TO Z)"', () => {
-
-
-    });
-
-    it('Successfully arranged the products from z to a after selecting the "Name (Z TO A)"', () => {
-
-
+        InventoryPage.backpackAddToCartElement.contains('Add to cart');
+        InventoryPage.backpackAddToCartElement.click();
+        InventoryPage.backpackRemoveElement.contains('Remove');
+        InventoryPage.backpackRemoveElement.click();
+        InventoryPage.backpackAddToCartElement.should(($buttonText) => {
+            expect($buttonText).to.contain('Add to cart');
+        });
     });
 
     it('Successfully displayed the shopping cart badge after clicking the "ADD TO CART"', () => {
@@ -49,4 +38,92 @@ describe('', () => {
 
 
     });
+});
+
+describe('Successfully sorted the products according to names in ascending or descending order', () => {
+    beforeEach(() => {
+        InventoryPage.visit();
+        cy.fixture('login').as('login')
+        cy.get<ValidCredentials>('@login').then((user) => {
+            cy.login(user.validUserName, user.validPassword);
+        });
+    });
+
+    it('Successfully sorted the products from a to z after selecting the "Name (A TO Z)"', () => {
+        let optionsArray = []
+        InventoryPage.sortElement.select(InventoryPage.sortNameAToZText);
+        InventoryPage.inventoryItemNamesElement.each(($el, index, list) => {
+            optionsArray[index] = $el.text();
+        }).then(() => {
+            expect(optionsArray).to.deep
+                .equal(['Sauce Labs Backpack', 'Sauce Labs Bike Light',
+                    'Sauce Labs Bolt T-Shirt', 'Sauce Labs Fleece Jacket',
+                    'Sauce Labs Onesie', 'Test.allTheThings() T-Shirt (Red)'])
+        });
+
+    });
+
+    it('Successfully sorted the products from z to a after selecting the "Name (Z TO A)"', () => {
+        let optionsArray = []
+        InventoryPage.sortElement.select(InventoryPage.sortNameZToAText);
+        InventoryPage.inventoryItemNamesElement.each(($el, index, list) => {
+            optionsArray[index] = $el.text();
+        }).then(() => {
+            expect(optionsArray).to.deep
+                .equal(['Test.allTheThings() T-Shirt (Red)', 'Sauce Labs Onesie', 'Sauce Labs Fleece Jacket',
+                    'Sauce Labs Bolt T-Shirt', 'Sauce Labs Bike Light', 'Sauce Labs Backpack'])
+
+        });
+
+    });
+
+});
+
+describe('Successfully sorted the products from low to high price after selecting the "Price (low TO high)"', () => {
+    beforeEach(() => {
+        InventoryPage.visit();
+        cy.fixture('login').as('login')
+        cy.get<ValidCredentials>('@login').then((user) => {
+            cy.login(user.validUserName, user.validPassword);
+        });
+    });
+
+    it('Check if the prices are sorted accordingly', () => {
+        let optionsArray = []
+        InventoryPage.sortElement.select(InventoryPage.sortPriceLowToHighText);
+        InventoryPage.inventoryItemPricesElement.each(($el, index, list) => {
+            optionsArray[index] = $el.text()
+            cy.log(optionsArray[index]);
+        }).then(() => {
+            expect(optionsArray).to.deep
+                .equal(['$7.99', '$9.99', '$15.99', '$15.99', '$29.99', '$49.99']);
+        });
+
+    });
+
+    it('Check if the names are sorted accordingly', () => {
+        let optionsArray = []
+        InventoryPage.sortElement.select(InventoryPage.sortPriceLowToHighText);
+        InventoryPage.inventoryItemNamesElement.each(($el, index, list) => {
+            optionsArray[index] = $el.text()
+            cy.log(optionsArray[index]);
+        }).then(() => {
+            expect(optionsArray).to.deep
+                .equal(['Sauce Labs Onesie', 'Sauce Labs Bike Light',
+                    'Sauce Labs Bolt T-Shirt', 'Test.allTheThings() T-Shirt (Red)',
+                    'Sauce Labs Backpack', 'Sauce Labs Fleece Jacket']);
+        });
+    });
+});
+
+describe('Successfully sorted the products from high to low price after selecting the "Price (high TO low)"', () => {
+    beforeEach(() => {
+        InventoryPage.visit();
+        cy.fixture('login').as('login')
+        cy.get<ValidCredentials>('@login').then((user) => {
+            cy.login(user.validUserName, user.validPassword);
+        });
+    });
+
+
 });
--- cypress/pages/Inventory.ts
+++ cypress/pages/Inventory.ts
@@ -1,6 +1,12 @@
 class Inventory {
 
+    //---Start - Sorting of products---//
     private sort: string = "select.product_sort_container";
+    private sortNameAToZ: string = "Name (A to Z)";
+    private sortNameZToA: string = "Name (Z to A)";
+    private sortPriceLowToHigh: string = "Price (low to high)";
+    private sortPriceHighToLow: string = "Price (high to low)";
+    //---End - Sorting of products---//
     //---Start - Sauce Labs Backpack---//
     private backpackPrice: string = ".inventory_item:nth-child(1) .inventory_item_price";
     private backpackTitle: string = ".inventory_item:nth-child(1) .inventory_item_name";
@@ -9,10 +15,34 @@ class Inventory {
     private backpackAddToCart: string = "#add-to-cart-sauce-labs-backpack";
     private backpackRemove: string = "#remove-sauce-labs-backpack";
     //---End - Sauce Labs Backpack---//
+    //---Start - inventory items---//
+    private inventoryItemNames: string = ".inventory_item_name";
+    private inventoryItemPrices: string = ".inventory_item_price";
+    //---End - inventory items---//
 
     //--Sort button--//
     get sortElement(): Cypress.Chainable<JQuery<HTMLElement>> {
         return cy.get(this.sort);
+    }
+
+    //--Sort Name (A to Z)--//
+    get sortNameAToZText(): string {
+        return this.sortNameAToZ;
+    }
+
+    //--Sort Name (Z to A)--//
+    get sortNameZToAText(): string {
+        return this.sortNameZToA;
+    }
+
+    //--Sort Price (low to high)--//
+    get sortPriceLowToHighText(): string {
+        return this.sortPriceLowToHigh;
+    }
+
+    //--Sort Price (high to low)--//
+    get sortPriceHighToLowText(): string {
+        return this.sortPriceHighToLow;
     }
 
     //--Backpack price text--//
@@ -43,6 +73,16 @@ class Inventory {
     //--Backpack Remove button--//
     get backpackRemoveElement(): Cypress.Chainable<JQuery<HTMLElement>> {
         return cy.get(this.backpackRemove);
+    }
+
+    //--Inventory Item names list--//
+    get inventoryItemNamesElement(): Cypress.Chainable<JQuery<HTMLElement>> {
+        return cy.get(this.inventoryItemNames);
+    }
+
+    //--Inventory Item prices list--//
+    get inventoryItemPricesElement(): Cypress.Chainable<JQuery<HTMLElement>> {
+        return cy.get(this.inventoryItemPrices);
     }
 
     visit(): void {
